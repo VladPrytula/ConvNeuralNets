fd09fedcf38e0cd19be33924cd51feb3f8d02757
Vladyslav Prytula <vladyslavprytula@Vladyslavs-MacBook-Pro.local>
2016-05-17 16:06:35 +0200

    affine backprop

--- assignment2/cs231n/layers.py
+++ assignment2/cs231n/layers.py
@@ -24,11 +24,7 @@ def affine_forward(x, w, b):
   # TODO: Implement the affine forward pass. Store the result in out. You     #
   # will need to reshape the input into rows.                                 #
   #############################################################################
-  print(x.shape)
-  print(w.shape)
-  print(b.shape)
   N = x.shape[0]
-  print(N)
   out = np.dot(x.reshape(N,-1),w) + b
   #############################################################################
   #                             END OF YOUR CODE                              #
@@ -57,7 +53,9 @@ def affine_backward(dout, cache):
   #############################################################################
   # TODO: Implement the affine backward pass.                                 #
   #############################################################################
-  pass
+  dx = np.dot(dout, w.T).reshape(x.shape)
+  dw = np.dot(x.reshape(x.shape[0],-1).T, dout)
+  db = np.dot(np.ones(dx.shape[0]),dout)
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
